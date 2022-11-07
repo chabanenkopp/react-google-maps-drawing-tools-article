5a8119038251275ec6573b30e35ec27711fdfc78
chabanenkopp <chabanenkopp@gmail.com>
2022-11-07 20:20:28 +0100

    Simplify project

--- src/routes/home/constants.ts
+++ src/routes/home/constants.ts
@@ -1,15 +1,8 @@
 import { Color } from "Theme";
-import { CIRCLE_CARD_WIDTH } from "./CircleCard";
 import { POLYGON_CARD_SIZE } from "./PolygonCard";
-import {
-  LINE_DISTANCE_CARD_WIDTH,
-  LineDistanceCardHeight,
-} from "./PolylineDistanceCard";
 
 const NATIVE_TRIANGLE_SIZE = 12;
-const CIRCLE_CARD_RIGHT_OFFSET = 26;
 const POLYGON_CARD_RIGHT_OFFSET = 20;
-const LINE_DISTANCE_CARD_RIGHT_OFFSET = 26;
 
 const PlusSymbol = {
   path: "M0,-1 V1 M-1,0 H1",
@@ -60,8 +53,8 @@ const PolylineOptions = {
 export const MapSettings = {
   MapConfig,
   PolygonOptions,
-  CircleOptions: PolygonOptions,
   PolylineOptions,
+  CircleOptions: PolygonOptions,
   InfoWindowOptions: {
     Triangle: {
       width: 13,
@@ -73,21 +66,6 @@ export const MapSettings = {
       CardPixelOffset: {
         X: POLYGON_CARD_SIZE / 2 + POLYGON_CARD_RIGHT_OFFSET,
         Y: (POLYGON_CARD_SIZE + NATIVE_TRIANGLE_SIZE) / 2,
-      },
-    },
-    Circle: {
-      CardPixelOffset: {
-        X: CIRCLE_CARD_RIGHT_OFFSET + CIRCLE_CARD_WIDTH / 2,
-        Y: -4,
-      },
-    },
-    LineDistance: {
-      CardPixelOffset: {
-        X: LINE_DISTANCE_CARD_RIGHT_OFFSET + LINE_DISTANCE_CARD_WIDTH / 2,
-        Y: (isTrajectory: boolean) =>
-          isTrajectory
-            ? LineDistanceCardHeight.Medium
-            : LineDistanceCardHeight.Small,
       },
     },
   },
--- src/routes/home/utils/getLineDistance.ts
+++
@@ -1,7 +0,0 @@
-interface Props {
-  to: google.maps.LatLngLiteral;
-  from: google.maps.LatLngLiteral;
-}
-
-export const getLineDistance = ({ from, to }: Props) =>
-  window.google.maps.geometry.spherical.computeDistanceBetween(from, to);
